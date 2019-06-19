106da59c1b50bc5586ab8d1b66371e4e2372cff4
jackyli <jackylijian@gmail.com>
2019-06-19 16:23:06 +0800

    Treasury Gameplan

--- wp-content/themes/Tradeshift/templates/single-resource2.php
+++ wp-content/themes/Tradeshift/templates/single-resource2.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Single Resources
+Template Name: Single Resources 2
 */
 get_header();
 ?>
@@ -31,24 +31,14 @@ get_header();
 				<div class="row">
 
 					<div class="resource-content">
-						<!--<div class="back-to-resource hidden-xs">
-							<a href="<?php if (ICL_LANGUAGE_CODE != 'en') echo '/' . ICL_LANGUAGE_CODE; ?>/resources">
+						<div class="back-to-resource hidden-xs">
+							<!--<a href="<?php if (ICL_LANGUAGE_CODE != 'en') echo '/' . ICL_LANGUAGE_CODE; ?>/resources">
 								<?php _e("Back to resource center", 'Tradeshift'); ?>
-							</a>
-						</div>-->
+							</a>-->
+						</div>
 						<?php the_post_thumbnail('full', array('class'=>'img-responsive')); ?>
-						<img width="650" height="350" src="https://tradeshiftchina.cn/wp-content/uploads/2019/03/game-plan-2019-topbanner.jpg" class="img-responsive wp-post-image" alt="" srcset="https://tradeshiftchina.cn/wp-content/uploads/2019/03/game-plan-2019-topbanner.jpg 650w, https://tradeshiftchina.cn/wp-content/uploads/2019/03/game-plan-2019-topbanner-250x135.jpg 250w" sizes="(max-width: 650px) 100vw, 650px" />
+						<img width="650" height="350" src="https://tradeshiftchina.cn/wp-content/uploads/2019/06/Treasury_Cover_2.png" class="img-responsive wp-post-image" alt="" srcset="https://tradeshiftchina.cn/wp-content/uploads/2019/06/Treasury_Cover_2.png 650w, https://tradeshiftchina.cn/wp-content/uploads/2019/03/Treasury_Cover_2.png 250w" sizes="(max-width: 650px) 100vw, 650px" />
 						<h2><?php the_title(); ?></h2>
-						<p><span style="font-weight: 400;">如今，科技发展已经能够让应付账款部门成为公司的战略数字中心，但大多数公司却依然在手动接收发票。这本电子书将指导您数字化应付账款部门，让它真正成为您期望中的金融信息中心。电子书中来自应付账款专业人员的真知灼见，将帮助您摆脱疲于应付各种文件的现状，迎接一个以数字化连接的2019。</span></p>
-						<p><span style="font-weight: 400;">在这本电子书中，您将获得以下专业建议：</span></p>
-						<ul>
- 							<li style="font-weight: 400;"><span style="font-weight: 400;">有效处理年终事务</span></li>
- 							<li style="font-weight: 400;"><span style="font-weight: 400;">制定技术路线图</span></li>
- 							<li style="font-weight: 400;"><span style="font-weight: 400;">化解传统财务部门的冗余工作</span></li>
- 							<li style="font-weight: 400;"><span style="font-weight: 400;">让您的整个企业保持数字化同步</span></li>
- 							<li style="font-weight: 400;"><span style="font-weight: 400;">使您的部门和事业与时俱进</span></li>
-						</ul>
-						<p><span style="font-weight: 400;">下载这本电子书，获得能够让您在2019蓬勃发展的制胜战术。</span></p>
 						<?php the_content(); ?>
 
 						<?php if ($video_link) : ?>
@@ -67,18 +57,12 @@ get_header();
 					<!-- Start right column contents - lead gen form and related materials -->
 					<div class="cta-form-related-material">
 						<!-- Lead Gen Form -->
-							<script src="//app-sjo.marketo.com/js/forms2/js/forms2.js"></script>
+						<?php if ($mkto_id) : ?>
 							<div class="cta-form">
 								<h3><?php _e("Get this resource", "Tradeshift"); ?></h3>
-
-								<form id="mktoForm_5421"></form>
-								<script type="text/javascript">
-						        MktoForms2.loadForm("//app-sjo.marketo.com", "213-SPR-003", 5421, function(form){
-						           form.vals({"Marketing_Resource_Name__c":"Game Plan 2019"});
-						    	});
-								</script>
+								<form id="mktoForm_<?php echo $mkto_id;?>"></form>
 							</div>
-
+						<?php endif; ?>
 
 						<!-- Related Materials -->
 						<?php
